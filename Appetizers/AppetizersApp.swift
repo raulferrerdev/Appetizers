707e8bbad5289a95908a6746bd03c7e504d0f0e8
Raúl Ferrer <10422910+raulferrerdev@users.noreply.github.com>
2021-09-19 14:36:08 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Raúl Ferrer on 19/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Raúl Ferrer on 19/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
