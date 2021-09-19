fa2ab801287603a03b32eea0dd3ae9e44dce1a1a
Raúl Ferrer <10422910+raulferrerdev@users.noreply.github.com>
2021-09-19 15:48:03 +0200

    NetworkManager

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Raúl Ferrer on 19/9/21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
