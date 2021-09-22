23ca8823577b5caa5d5712ec89664bd867b2cfb3
Raúl Ferrer <10422910+raulferrerdev@users.noreply.github.com>
2021-09-22 08:22:20 +0200

    LoadingView

---
+++ Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Raúl Ferrer on 22/9/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
